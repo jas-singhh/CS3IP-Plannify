3ece42c4042172ff072333a21e410e02dd205d8f
Jas <jaswag99@gmail.com>
2024-01-12 12:05:11 +0000

    Add homepage chip group with associated on click listeners

---
+++ app/src/main/java/uk/ac/aston/cs3mdd/mealplanner/repos/QuotesRepository.java
@@ -0,0 +1,22 @@
+package uk.ac.aston.cs3mdd.mealplanner.repos;
+
+import java.util.List;
+
+import retrofit2.Call;
+import uk.ac.aston.cs3mdd.mealplanner.api.QuoteService;
+import uk.ac.aston.cs3mdd.mealplanner.data.quote.Quote;
+
+public class QuotesRepository {
+
+    // This class will store the data retrieved by the API request
+    // Reference: CS3MDD Lab 4
+
+    private QuoteService request;
+
+    public QuotesRepository(QuoteService request) {this.request = request;}
+
+    public Call<List<Quote>> getQuote() {
+        return request.getQuotes();
+    }
+
+}
